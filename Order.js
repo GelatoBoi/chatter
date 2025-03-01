59e71e3609f8ac4cf8946f3d8ab9a959ab960d51
GelatoBoi <144965276+GelatoBoi@users.noreply.github.com>
2025-03-01 03:04:40 +0000

    Did all the ordering

--- Order.js
+++ Order.js
@@ -4,24 +4,95 @@ export class Order {
         WELCOMING: () => {
           let aReturn = [];
           this.stateCur = this.OrderState.RESERVING;
-          aReturn.push("Welcome to Rich's Acton Rapid Test.");
-          aReturn.push("Would you like to reserve a rapid test kit?");
+          aReturn.push("Welcome to Areeb's Biryani Bar");
+          aReturn.push("Would you like to reserve or takeout?");
           return aReturn;
         },
+
+
         RESERVING: (sInput) => {
           let aReturn = [];
           this.isDone = true;
-          if (sInput.toLowerCase().startsWith('y')) {
-            aReturn.push(`Your rapid test is reserved under the phone number ${this.sFrom}`);
+
+          //Reserve
+          if (sInput.toLowerCase().startsWith('r')) {
+            aReturn.push(`Your reservation table is reserved under the phone number ${this.sFrom}`);
             let d = new Date();
             d.setMinutes(d.getMinutes() + 120);
-            aReturn.push(`Please pick it up at 123 Tidy St., Acton before ${d.toTimeString()}`);
+            aReturn.push(`Please come to your table at 67 Darling St, Brantford., Acton before ${d.toTimeString()}`);
+
+          } 
+          
+          //Takeout
+          if (sInput.toLowerCase().startsWith('t')) {
+            aReturn.push(`Would you like Chicken Biryani, Beef Biryani, or Fish Biryani?`);
+
+
+
+            if (sInput.toLowerCase().startsWith('c')) {
+              aReturn.push(`Would you like a Pepsi with that?`);
+
+              if (sInput.toLowerCase().startsWith('y')) {
+                aReturn.push(`Thank you for ordering. `);
+                let d = new Date();
+                d.setMinutes(d.getMinutes() + 120);
+                aReturn.push(`Please come pick up your order at 67 Darling St, Brantford., Acton before ${d.toTimeString()}`);
+  
+              } else {
+                aReturn.push("Error: Wrong input. Try ordering again...");
+              }
+
+            } else {
+              aReturn.push("Error: Wrong input. Try ordering again...");
+            }
+
+
+
+            if (sInput.toLowerCase().startsWith('b')) {
+              aReturn.push(`Would you like a Pepsi with that?`);
+
+              if (sInput.toLowerCase().startsWith('y')) {
+                aReturn.push(`Thank you for ordering. `);
+                let d = new Date();
+                d.setMinutes(d.getMinutes() + 120);
+                aReturn.push(`Please come pick up your order at 67 Darling St, Brantford., Acton before ${d.toTimeString()}`);
+  
+              } else {
+                aReturn.push("Error: Wrong input. Try ordering again...");
+              }
+
+            } else {
+              aReturn.push("Error: Wrong input. Try ordering again...");
+            }
+
+
+
+            if (sInput.toLowerCase().startsWith('f')) {
+              aReturn.push(`Would you like a Pepsi with that?`);
+
+              if (sInput.toLowerCase().startsWith('y')) {
+                aReturn.push(`Thank you for ordering. `);
+                let d = new Date();
+                d.setMinutes(d.getMinutes() + 120);
+                aReturn.push(`Please come pick up your order at 67 Darling St, Brantford., Acton before ${d.toTimeString()}`);
+  
+              } else {
+                aReturn.push("Error: Wrong input. Try ordering again...");
+              }
+
+            } else {
+              aReturn.push("Error: Wrong input. Try ordering again...");
+            }
+            
+
           } else {
             aReturn.push("Thanks for trying our reservation system");
             aReturn.push("Maybe next time");
           }
           return aReturn;
         }
+
+
       };
   
       this.stateCur = this.OrderState.WELCOMING;
